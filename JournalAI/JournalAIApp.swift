8b84869388c496d555fa1bf28d64031431080e9d
asheshp23 <asheshp23@gmail.com>
2025-06-11 16:48:47 -0400

    Initial Commit

---
+++ JournalAI/JournalAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAIApp.swift
+//  JournalAI
+//
+//  Created by Ashesh Patel on 2025-06-11.
+//
+
+import SwiftUI
+
+@main
+struct JournalAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
